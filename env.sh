a4c2bf5cae62da1c7c7fd65a844257247f6c27f9
Bryan Luce <luce@appmelia.com>
2025-11-26 21:48:08 -0600

    Enhance env.sh to print current PATH

--- env.sh
+++ env.sh
@@ -5,3 +5,5 @@
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export PATH="${PROJECT_ROOT}:${PATH}"
 echo "PATH updated for project: ${PROJECT_ROOT}"
+echo "Current PATH:"
+echo "${PATH}" | tr ':' '\n'
